4db5c99756dc8299bdd2058ddd0097da7107e137
mihaylova-l <lolabudko@gmail.com>
2024-04-08 18:04:27 +0300

    add logger

--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -1,7 +1,7 @@
 import pytest
-import requests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
+from lib.my_requests import MyRequests
 
 class TestUserAuth(BaseCase):
     exclude_params = [
@@ -14,15 +14,15 @@ class TestUserAuth(BaseCase):
             'password': '1234'
         }
 
-        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
+        response1 = MyRequests.post("/user/login", data=data)
 
         self.auth_sid = self.get_cookie(response1, "auth_sid")
         self.token = self.get_header(response1, "x-csrf-token")
         self.user_id_from_auth_method = self.get_json_value(response1, "user_id")
 
     def test_auth_user(self):
-        response2 = requests.get(
-            "https://playground.learnqa.ru/api/user/auth",
+        response2 = MyRequests.get(
+            "/user/auth",
             headers={"x-csrf-token": self.token},
             cookies={"auth_sid": self.auth_sid}
         )
@@ -39,14 +39,14 @@ class TestUserAuth(BaseCase):
         def test_auth_user(self):
 
             if condition =="no_cookie":
-                response2 = requests.get(
-                    "https://playground.learnqa.ru/api/user/auth",
+                response2 = MyRequests.get(
+                    "/user/auth",
                     headers={"x-csrf-token": self.token},
                     cookies={"auth_sid": self.auth_sid}
                 )
             else:
-                    response2 = requests.get(
-                        "https://playground.learnqa.ru/api/user/auth",
+                    response2 = MyRequests.get(
+                        "/user/auth",
                         cookies={"auth_sid": self.auth_sid}
                     )
 
--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,7 +1,6 @@
-import requests
+from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
-from datetime import datetime
 import pytest
 import random
 import string
@@ -15,52 +14,28 @@ class TestUserRegister(BaseCase):
         ('email')
     ]
 
-    def setup_method(self):
-        base_url = "learnqa"
-        domain = "example.com"
-        random_part = datetime.now().strftime("%m%d%Y%H%M%S")
-        self.email = f"{base_url}{random_part}@{domain}"
-
     def test_create_user_successfully(self):
-        data = {
-            'password': '123',
-            'username': 'learnqa',
-            'firstName': 'learnqa',
-            'lastName': 'learnqa',
-            'email': self.email
-        }
+        data = self.prepare_registration_data()
 
-        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        response = MyRequests.post("/user/", data=data)
 
         Assertions.assert_code_status(response, 200)
         Assertions.assert_json_has_key(response, "id")
 
     def test_create_user_with_existing_email(self):
         email = 'vinkotov@example.com'
-        data = {
-            'password': '123',
-            'username': 'learnqa',
-            'firstName': 'learnqa',
-            'lastName': 'learnqa',
-            'email': email
-        }
+        data = self.prepare_registration_data(email)
 
-        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        response = MyRequests.post("/user/", data=data)
 
         Assertions.assert_code_status(response, 400)
         assert response.content.decode("utf-8") == f"Users with email '{email}' already exists", f"Unexpected response content {response.content}"
 
     def test_create_user_with_incorrect_email(self):
         email = 'vinkotovexample.com'
-        data = {
-            'password': '123',
-            'username': 'learnqa',
-            'firstName': 'learnqa',
-            'lastName': 'learnqa',
-            'email': email
-        }
+        data = self.prepare_registration_data(email)
 
-        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        response = MyRequests.post("/user/", data=data)
 
         Assertions.assert_code_status(response, 400)
         assert response.content.decode(
@@ -68,45 +43,30 @@ class TestUserRegister(BaseCase):
 
     @pytest.mark.parametrize('param', params)
     def test_null_param(self, param):
-        data = {'password': '123', 'username': 'learnqa', 'firstName': 'learnqa', 'lastName': 'learnqa',
-                'email': "vinkotovexample.com"}
-
+        data = self.prepare_registration_data()
         data[param] = None
 
-        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        response = MyRequests.post("/user/", data=data)
 
         Assertions.assert_code_status(response, 400)
         assert response.content.decode(
             "utf-8") == f"The following required params are missed: {param}", f"Unexpected response content {response.content}"
 
-
     def test_create_user_short_firstrname(self):
-        firstname = "".join([random.choice(string.ascii_letters) for i in range(random.randrange(2))])
-        data = {
-            'password': '123',
-            'username': 'learnqa',
-            'firstName': firstname,
-            'lastName': 'learnqa',
-            'email': self.email
-        }
+        data = self.prepare_registration_data()
+        data['firstName'] = "".join([random.choice(string.ascii_letters) for i in range(random.randrange(2))])
 
-        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        response = MyRequests.post("/user/", data=data)
 
         Assertions.assert_code_status(response, 400)
         assert response.content.decode(
             "utf-8") == f"The value of 'firstName' field is too short", f"Unexpected response content {response.content}"
 
     def test_create_user_long_firstname(self):
-        firstname = "".join([random.choice(string.ascii_letters) for i in range(random.randrange(500))])
-        data = {
-            'password': '123',
-            'username': 'learnqa',
-            'firstName': firstname,
-            'lastName': 'learnqa',
-            'email': self.email
-        }
+        data = self.prepare_registration_data()
+        data['firstName'] = "".join([random.choice(string.ascii_letters) for i in range(random.randrange(500))])
 
-        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        response = MyRequests.post("/user/", data=data)
 
         Assertions.assert_code_status(response, 400)
         assert response.content.decode(
